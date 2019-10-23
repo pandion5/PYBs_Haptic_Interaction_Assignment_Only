b3fd1b1305016db1c8e5243586262431526cc090
Shifty <note3352@gmail.com>
2019-10-23 14:05:30 +0900

    Update 02_4_SerialSendToPC.ino

--- 01_Arduino/02_4_SerialSendToPC.ino
+++ 01_Arduino/02_4_SerialSendToPC.ino
@@ -6,11 +6,11 @@ void setup() {
 // 1초마다 PC로 숫자를 보내는 예제
 void loop() {
   char buf[255];
-  sprintf(buf, "%d", cnt1);
+  sprintf(buf, "%d", cnt1); //버퍼에 cnt1저장
   // "%d" 를 "%c" 로 바꾸면 어떻게 될까요?
+  // c로 바꾸면 아스키 코드에 해당하는 문자 출력
+  Serial.println(buf); // 버퍼에 있는 문자 출력
+  cnt1++; //cnt1 증가
   
-  Serial.println(buf);
-  cnt1++;
-  
-  delay(1000);
+  delay(1000); // 1초 경과 후 루프
 }
